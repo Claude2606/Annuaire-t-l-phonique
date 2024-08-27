e3e5825766c9bdb2c9bcd6b4ae053ce4fea39e06
Claude2606 <claude.peltier@apprenant.ifapme.be>
2024-08-27 15:12:56 +0200

    ajout icon onglet

--- view/pconnexion.php
+++ view/pconnexion.php
@@ -13,6 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" type="annuaire téléphonique" href="../assets/icon.png">
     <title>Connexion</title>
 </head>
 <body>
--- view/pinscription.php
+++ view/pinscription.php
@@ -7,6 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" type="annuaire téléphonique" href="../assets/icon.png">
     <title>Inscription</title>
 </head>
 <body>
